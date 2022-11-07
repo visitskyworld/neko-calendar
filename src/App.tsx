4f1d58470d699ca487bb762adae0092234522b30
visitskyworld <visitskyworld@gmail.com>
2022-11-07 09:57:32 +0100

    renamed: pages uppercase

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 import Layout from './layout/Main';
-import CalendarView from './pages/CalendarView';
-import SpreadsheetView from './pages/SpreadsheetView';
-import DatabaseView from './pages/DatabaseView';
+import CalendarView from './pages/calendarView';
+import SpreadsheetView from './pages/spreadsheetView';
+import DatabaseView from './pages/databaseView';
 import HomeView from './pages/HomeView';
 
 import './styles/index.scss';
